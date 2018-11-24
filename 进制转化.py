5c25da0edce1104de56e52a0b326b3d681938d05
lin923 <596535754@qq.com>
2018-11-24 09:21:44 +0800

    practice

---
+++ 进制转化.py
@@ -0,0 +1,15 @@
+##内置函数进制转化
+import math
+bin() ##二进制转化
+oct() ##八进制转化
+hex() ##十六进制转化
+
+##print函数转化
+print("%o") ##八进制输出
+print("%x") ##十六进制输出
+
+##format()函数转化
+format(x, 'b') ##二进制
+format(x, 'o') ##八进制
+format(x, 'x') ##十六进制
+
